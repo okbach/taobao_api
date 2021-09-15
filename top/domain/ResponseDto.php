26ab397df92adf61ea07ce6b44690ecc64dc7e80
okba <33584599+okbach@users.noreply.github.com>
2021-09-15 10:51:12 -0700

    Add files via upload

---
+++ top/domain/ResponseDto.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 返回结果
+ * @author auto create
+ */
+class ResponseDto
+{
+	
+	/** 
+	 * 返回结果状态码
+	 **/
+	public $resp_code;
+	
+	/** 
+	 * 返回结果状态描述
+	 **/
+	public $resp_msg;
+	
+	/** 
+	 * 返回结果明细
+	 **/
+	public $result;	
+}
+?>
